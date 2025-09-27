711b9460726e59e1304732e8b0e1e64f10c5d836
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-09-27 10:30:54 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+MouseX::OO\_Modulino - It's new $module
+
+# SYNOPSIS
+
+    use MouseX::OO_Modulino;
+
+# DESCRIPTION
+
+MouseX::OO\_Modulino is ...
+
+# LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kobayasi, Hiroaki <buribullet@gmail.com>
---
+++ lib/MouseX/OO_Modulino.pm
@@ -0,0 +1,39 @@
+package MouseX::OO_Modulino;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MouseX::OO_Modulino - It's new $module
+
+=head1 SYNOPSIS
+
+    use MouseX::OO_Modulino;
+
+=head1 DESCRIPTION
+
+MouseX::OO_Modulino is ...
+
+=head1 LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kobayasi, Hiroaki E<lt>buribullet@gmail.comE<gt>
+
+=cut
+
